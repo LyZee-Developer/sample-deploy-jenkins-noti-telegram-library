e95510ee30500f45b1957c970d4bb317df81b694
leangsengLy <edeepleang@gmail.com>
2026-01-08 13:40:10 +0700

    add build deploy

--- vars/SendMessageToTelegram.groovy
+++ vars/SendMessageToTelegram.groovy
@@ -1,7 +1,7 @@
 def call(String message,String Chat_Id,String Token){
     def msg = "${message}"
     sh """
-        curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        curl -s -X POST "https://api.telegram.org/bot${Token}/sendMessage" \
         -d chat_id=${Chat_Id} \
         -d text="${msg}" \
         -d parse_mode=MarkdownV2
